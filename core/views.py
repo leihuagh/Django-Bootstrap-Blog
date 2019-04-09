a9334ff15f3153de1c160a8306c60c9bc65b3776
Lei Hua <lhua@example.com>
2019-04-08 20:43:47 -0400

    multi-views share template

--- core/views.py
+++ core/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'index.html', {})
+    context = {
+        'title': 'Welcome',
+        'page_title': 'Home Index',
+    }
+    return render(request, 'index.html', context)
+
+
+def about(request):
+    context = {
+        'title': 'About Us',
+        'page_title': 'About Us Page',
+    }
+    return render(request, 'index.html', context)
+
+
+def contact(request):
+    context = {
+        'title': 'Contact Us',
+        'page_title': 'Contact Us Page',
+    }
+    return render(request, 'index.html', context)
