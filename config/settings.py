42c0b0237d21f242f1d01e71a8262e00e5067697
Lei Hua <lhua@example.com>
2019-04-12 18:59:10 -0400

    design my account

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'core.apps.CoreConfig',
     'authentication.apps.AuthenticationConfig',
     'home.apps.HomeConfig',
