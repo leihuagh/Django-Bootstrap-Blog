337735b7c0631b25f94d31c7a9d0710c27c1990d
Lei Hua <lhua@example.com>
2019-04-13 14:26:13 -0400

    enums

---
+++ core/enums/GenderEnum.py
@@ -0,0 +1,7 @@
+from .ChoiceEnum import ChoiceEnum
+
+
+class GenderEnum(ChoiceEnum):
+    male = 'Male',
+    female = 'Female',
+    other = 'Other',
---
+++ core/enums/__init__.py
@@ -0,0 +1,11 @@
+from .ChoiceEnum import ChoiceEnum
+from .PublishStatusEnum import PublishStatusEnum
+from .StateEnum import StateEnum
+from .GenderEnum import GenderEnum
+
+__all__ = [
+    'ChoiceEnum',
+    'PublishStatusEnum',
+    'StateEnum',
+    'GenderEnum',
+]
