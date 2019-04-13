5216d67b50f01e1083e19a6c6a46967384464089
Lei Hua <lhua@example.com>
2019-04-12 21:24:45 -0400

    register form

--- users/views.py
+++ users/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from .forms import UserRegisterForm
 
 
 def register(request):
-    return render(request, 'users/register.html', {})
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+    else:
+        form = UserRegisterForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
